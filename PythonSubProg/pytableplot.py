a4c0f3da10ba693572908596fc7cf95c784e2c61
ST1 <141134649+STopich1@users.noreply.github.com>
2025-10-06 00:26:46 +0300

    Ну допустим
    Поработал в файле pytableplot но еще нужно будет тестить и отлаживать, я думаю.
    
    доделал до адекватного состояния (более менее) файл что раньше лежал в папке cms
    
    а еще по идеи поставил в проект Pandas и openpyxl.

--- PythonSubProg/pytableplot.py
+++ PythonSubProg/pytableplot.py
@@ -1,11 +1,22 @@
 import sys
+from XlsArrayer import XlsArrayerOut
+from tabledifffun import tables_differences
 
 def main():
     filename = ""
     while True:
-        line = sys.stdin.readline().strip()
-        if line.startswith("FILE="):
+        line = sys.stdin.readline().strip() # чтение строки
+        if line.startswith("FILE="): 
             filename = line[5:]
+        elif line == "SAVE!":
+            SavedTable = ''
+            SavedTable = XlsArrayerOut(filename)
+            print ("saved", SavedTable, flush=True)
+        elif line == "COMPARE!":
+            if SavedTable == '':
+                print("no saved previous result(nothing to compare)", flush=True)
+            else:
+                tables_differences(SavedTable, XlsArrayerOut(filename), True, flush=True) #настроен на печать вывода 
         elif line == "CHECK!":
             print (filename, flush=True)
         else:
