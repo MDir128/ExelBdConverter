607b4b3a3a21fb4a759cb350294a038b4c2932cb
STopich1 <141134649+STopich1@users.noreply.github.com>
2025-10-26 22:14:51 +0300

    Reapply "Reapply "Merge branch 'main' of https://github.com/MDir128/ExelBdConverter""
    This reverts commit 310d23c217984ad663b18def09e4e0e83425f75c.

--- PythonSubProg/pytableplot.py
+++ PythonSubProg/pytableplot.py
@@ -2,25 +2,40 @@ import sys, os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, script_dir)
 from tabledifffun import *
-from XlsArrayer import *
+##from XlsArrayer import *
 def main():
+    ##!!!!
+    ## В print() всегда возвращаем flag+'$'+(ответ), если на вход был флаг (а он должен быть ВСЕГДА!)
+    ##!!!!
     filename = ""
     while True:
         line = sys.stdin.readline().strip() # чтение строки
-        if line.startswith("FILE="): 
-            filename = line[5:]
-        elif line == "SAVE!":
-            SavedTable = ''
-            SavedTable = XlsArrayerOut(filename)
-            print ("saved", SavedTable, flush=True)
-        elif line == "COMPARE!":
-            if SavedTable == '':
-                print("no saved previous result(nothing to compare)", flush=True)
-            else:
-                print(tables_differences(SavedTable, XlsArrayerOut(filename), False), flush=True) #настроен на печать вывода 
-        elif line == "CHECK!":
-            print (filename, flush=True)
+        splitedline = line.split("$")
+        flag = "none"
+        if len(splitedline) == 2:
+            flag = splitedline[0]
+            line = splitedline[1]
+        if flag=='SetFILE': 
+            filename = line
+        ##elif line == "SAVE!":
+        ##    SavedTable = ''
+        ##    SavedTable = XlsArrayerOut(filename)
+        ##    print ("saved", SavedTable, flush=True)
+        ##elif line == "COMPARE!":
+        ##    if SavedTable == '':
+        ##        print("no saved previous result(nothing to compare)", flush=True)
+        ##    else:
+        ##        print(tables_differences(SavedTable, XlsArrayerOut(filename), False), flush=True) #настроен на печать вывода 
+        elif flag=='Debug' and line == "CHECK!":
+            i = 0
+            n = 1
+            for i in range(1, 10000):
+                if i%3==0:
+                    n//=3
+                else:
+                    n*=2
+            print (flag+'$'+filename, n, flush=True)
         else:
-            print("uncknown command", flush=True)
+            print(flag+'$'+"uncknown command", flush=True)
 if __name__ == "__main__":
     main()
