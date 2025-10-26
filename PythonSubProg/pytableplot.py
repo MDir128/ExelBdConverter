dbf4a53c78087e09f874e1efafcc6c593d95352e
STopich1 <141134649+STopich1@users.noreply.github.com>
2025-10-26 20:47:11 +0300

    Отче наш иже еси...
    Много экспериментов. Объявляю python код независимым от влияния Андрея. (Я там 100% все сломал).
    
    парочка переделок. мои навыки давно закончились так что тут почти чистый гпт. даже не знаю стыдить этого или уже пофиг.
    
    я ставлю плейлисты на 4 часа, а потом удивляюсь что они закончились а я все еще на том же месте где начал

--- PythonSubProg/pytableplot.py
+++ PythonSubProg/pytableplot.py
@@ -1,6 +1,7 @@
 import sys, os
 script_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, script_dir)
+sys.path.insert(0, os.path.join(script_dir))
+
 from tabledifffun import *
 from XlsArrayer import *
 def main():
