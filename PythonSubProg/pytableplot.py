aecd308fdd0306ed9f16d2c6f93d992706c9fd9b
STopich1 <141134649+STopich1@users.noreply.github.com>
2025-10-26 22:04:26 +0300

    Revert "Отче наш иже еси..."
    This reverts commit dbf4a53c78087e09f874e1efafcc6c593d95352e.

--- PythonSubProg/pytableplot.py
+++ PythonSubProg/pytableplot.py
@@ -1,7 +1,6 @@
 import sys, os
 script_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(script_dir))
-
+sys.path.insert(0, script_dir)
 from tabledifffun import *
 ##from XlsArrayer import *
 def main():
